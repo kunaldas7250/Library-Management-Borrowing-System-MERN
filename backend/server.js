d5c78233f7e30d8afde47ed9793c8cc1d6cabaf9
it's kunal_riku <140656671+kunaldas7250@users.noreply.github.com>
2026-01-12 06:27:03 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,51 +1,3 @@
-// import express from "express";
-// import mongoose from "mongoose";
-// import cors from "cors";
-// import dotenv from "dotenv";
-// import authRoutes from "./routes/auth.js";
-// import userRoutes from "./routes/users.js";
-// import bookRoutes from "./routes/books.js";
-// import transactionRoutes from "./routes/transactions.js";
-// import categoryRoutes from "./routes/categories.js";
-// import { seedBooks } from "./data/seedBooks.js";
-// /* App Config */
-// dotenv.config();
-// const app = express();
-// const port = process.env.PORT || 4000;
-
-// /* Middlewares */
-// app.use(express.json());
-// app.use(cors());
-
-// /* API Routes */
-// app.use("/api/auth", authRoutes);
-// app.use("/api/users", userRoutes);
-// app.use("/api/books", bookRoutes);
-// app.use("/api/transactions", transactionRoutes);
-// app.use("/api/categories", categoryRoutes);
-
-// /* MongoDB connection */
-// mongoose.connect(
-//   process.env.MONGO_URL,
-//   {
-//     useCreateIndex: true,
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   },
-//   () => {
-//     console.log("MONGODB CONNECTED");
-//   }
-// );
-// seedBooks();
-// app.get("/", (req, res) => {
-//   res.status(200).send("Welcome to LibraryApp");
-// });
-
-// /* Port Listening In */
-// app.listen(port, () => {
-//   console.log(`Server is running in PORT ${port}`);
-// });
-
 
 import express from "express";
 import mongoose from "mongoose";
@@ -64,11 +16,11 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 4000;
 
-/* Middlewares */
+
 app.use(express.json());
 app.use(cors());
 app.use("/api/borrow", borrowRoutes);
-/* Routes */
+
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/books", bookRoutes);
@@ -79,7 +31,7 @@ app.get("/", (req, res) => {
   res.status(200).send("Welcome to Smart Library App");
 });
 
-/* MongoDB Connection */
+
 mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
@@ -87,7 +39,7 @@ mongoose
   })
   .then(async () => {
     console.log("MONGODB CONNECTED");
-    await seedBooks(); // 🔥 Insert predefined 20 books
+    await seedBooks();
   })
   .catch((err) => console.log(err));
 
