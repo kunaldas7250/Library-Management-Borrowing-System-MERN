fc129a77068c1acccdde42938df97eba087abf80
it's kunal_riku <140656671+kunaldas7250@users.noreply.github.com>
2026-01-12 06:20:49 +0530

    Update AuthContext.js

--- frontend/src/Context/AuthContext.js
+++ frontend/src/Context/AuthContext.js
@@ -7,11 +7,10 @@ const INITIAL_STATE = {
     error:false
 }
 
-/* Reads the data from the Provider and changes INITIAL_STATE */
+
 export const AuthContext = createContext(INITIAL_STATE)
 
-/* Children here are the Components that need to get the data.[In this Application we specified App COmponent as Child in index.js so that we can server every every component exist in the app */
-/* This will provide data to all the children that we are giving here */
+
 export const AuthContextProvider = ({children}) =>{
     const [state, dispatch] = useReducer(AuthReducer, INITIAL_STATE);
 
